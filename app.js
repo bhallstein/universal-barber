99462675ae42f7a180971291a6f7e52f559bd2e5
Ben <ben@Fratercula.local>
2017-12-30 18:46:27 +0000

    Sass compilation

---
+++ app.js
@@ -0,0 +1,6 @@
+//
+// app.js - create webpack bundle
+//
+
+require('./src/universal-barber.js');
+require('./src/universal-barber.scss');
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const html_plugin = require('html-webpack-plugin');
+const extract_text_plugin = require('extract-text-webpack-plugin');
 
 const config = {
   entry: {
-    app: path.resolve(__dirname, 'src/app.js'),
+    app: path.resolve(__dirname, 'app.js'),
   },
   output: {
     filename: 'app.js',
@@ -19,10 +20,18 @@ const config = {
           presets: ['es2015'],
         },
       },
+      {
+        test: /\.scss$/,
+        use: extract_text_plugin.extract({
+          fallback: 'style-loader',
+          use:      ['css-loader', 'sass-loader'],
+        }),
+      },
     ],
   },
   plugins: [
-    new html_plugin({ template: path.resolve(__dirname, 'src/app.html') }),
+    new html_plugin({ template: path.resolve(__dirname, 'src/universal-barber.html') }),
+    new extract_text_plugin({ filename:'app.css' }),
   ],
 };
 
