ac48283d2468d0f9d77c01e8404a54b04698f579
Ben Hallstein <benhall@brandwatch.com>
2019-01-16 10:13:41 +0000

    update tooling

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,56 @@
 const path = require('path');
-const html_plugin = require('html-webpack-plugin');
-const extract_text_plugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+const html_webpack_plugin = require('html-webpack-plugin');
+const mini_css_extract_plugin = require('mini-css-extract-plugin'); // TODO: remove w/ webpack 5
 
 const config = {
+  mode: 'production',
+
   entry: {
     app: path.resolve(__dirname, 'app.js'),
   },
+
   output: {
     filename: 'app.js',
     path: path.resolve(__dirname, 'dist'),
   },
+
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
         loader: 'babel-loader',
-        query: {
-          presets: ['es2015'],
-        },
+        exclude: /node_modules/,
       },
       {
         test: /\.scss$/,
-        use: extract_text_plugin.extract({
-          fallback: 'style-loader',
-          use:      ['css-loader', 'sass-loader'],
-        }),
+        use: [
+          mini_css_extract_plugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+                plugins: require('cssnano'),
+            },
+          },
+          'fast-sass-loader',
+        ],
       },
     ],
   },
+
   plugins: [
-    new html_plugin({ template: path.resolve(__dirname, 'src/universal-barber.html') }),
-    new extract_text_plugin({ filename:'app.css' }),
+    new html_webpack_plugin({ template: path.resolve(__dirname, 'src/universal-barber.html') }),
+    new mini_css_extract_plugin({
+      filename: 'app.css',
+      sourceMap: false,
+    }),
+    new webpack.SourceMapDevToolPlugin({
+      filename: 'app.js.map',
+      test: /\.js$/,
+    }),
   ],
 };
 
 module.exports = config;
+
