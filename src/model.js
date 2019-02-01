abc09cc1f02186ceafe48cf42a2ed955235afaba
Ben Hallstein <a@ben.am>
2019-02-01 19:33:20 +0000

    model save/load to localStorage

--- src/model.js
+++ src/model.js
@@ -5,11 +5,45 @@
 import Big from 'big.js';
 
 
+function is_bigjs(str) {
+  return (
+    typeof v === 'string' &&
+    v.match(/^-?\d+(?:\.\d+)?(?:e[+\-]?\d+)?$/i)
+  );
+}
+
+
+function save() {
+  try {
+    localStorage.setItem('universal-barber', JSON.stringify(state));
+  }
+  catch (error) {
+    // Unable to store
+  }
+}
+
+
+function load() {
+  try {
+    const data = JSON.parse(localStorage.getItem('universal-barber'), (k, v) => {
+      return is_bigjs(v) ? new Big(v) : v;
+    });
+    state = data;
+  }
+  catch (error) {
+    // Unable to load
+  }
+}
+
+
 let state = {
   haircuts: Big(0),
-	shaves: Big(0),
+  shaves: Big(0),
   money: Big(0),
+
+  save,
 };
+
 
 export default state;
 
